1531ef9aeabe13429945db4813cc654b463a3c9b
simonulvevadet <simonulvevadet@gmail.com>
2025-05-07 10:06:01 +0200

    navbar styling og navigering
    fjernet mye midlertidig kode som css i html. rettet litt i navbar og linket opp kontakt siden samt å gi navbaren bedre beiledende navn. stylet navbar

--- Kode/script.js
+++ Kode/script.js
@@ -1,3 +1,17 @@
 function sendMelding() {
     alert("Melding sendt!");
 }
+
+function darkmode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+document.getElementById('comment-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const input = document.getElementById('comment').value;
+    const commentSection = document.getElementById('comment-section');
+    const p = document.createElement('p');
+    p.textContent = input;
+    commentSection.appendChild(p);
+    document.getElementById('comment').value = '';
+});
